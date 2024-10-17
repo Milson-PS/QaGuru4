0080c039faf0d0914aff22fad9d937e278a8eeea
pavelmilyukov <Milson7017@yandex.ru>
2024-10-17 09:43:08 +0300

    dz ok

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,35 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.byText; // Импорт для byText
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.Condition;
+
+public class SearchTests {
+    @BeforeAll
+    static void BeforeALL() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void FillFormTest() {
+        open("/selenide/selenide");
+        $("span[data-content='Wiki']").click();
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
